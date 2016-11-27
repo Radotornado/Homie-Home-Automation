81220b76d7b940c6f0ddb7093592cc2e0ae66a9e
Radotornado <radoslav.mandev99@gmail.com>
2016-11-27 21:49:51 +0200

    added comments

--- GasSensor.ino
+++ GasSensor.ino
@@ -1,9 +1,15 @@
+// A program to send data to Raspberry PI using serial
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Start the serial monitor
 }
+
+//Loop
 void loop() {
+  // Assigning a variable that contains the data from the gas sensor
   int sersorValue;
+  // Reads the data through analog pin
   sersorValue = analogRead(A0);
+  // Checks the value if its higher than a certain level and sends them to the RPI
   if(sersorValue >= 150)
   {
     Serial.print('1');
